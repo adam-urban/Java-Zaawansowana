ec2867b5fdd47f73c2692c443edfdad94128fa59
adam-urban <adam.jan.urban@gmail.com>
2022-10-14 00:37:34 +0200

    Pierwszy test

--- Lab01/src/ApplicationLab01.java
+++ Lab01/src/ApplicationLab01.java
@@ -3,6 +3,9 @@ package Lab01.src;
 public class ApplicationLab01 {
     
     public static void main(String[] args) {
-        
+        KoktajlTruskawkowy k = new KoktajlTruskawkowy();
+
+        k.pij();
+
     }
 }
