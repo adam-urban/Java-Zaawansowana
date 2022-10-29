778479ccea33f51738e07761a933cebdfa037347
adurban <adam.jan.urban@gmail.com>
2022-10-29 16:21:54 +0200

    Rozwiazanie Zadania w klasie Koktajl Smakowy - zrodloSmaku

--- Lab01_02/src/ApplicationLab01.java
+++ Lab01_02/src/ApplicationLab01.java
@@ -4,10 +4,10 @@ public class ApplicationLab01 {
     
     public static void main(String[] args) {
         
-        KoktajlSmakowy k = new KoktajlSmakowy(20, RodzajSmaku.BANANOWY);
+        KoktajlSmakowy k = new KoktajlSmakowy(20, RodzajSmaku.SZPINAKOWY);
 
         k.pij();
-        System.out.println(k.smak());
+
 
     }
 }
